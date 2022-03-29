06b54ffbd3ed2afa3c75fdaf940adcccac6f9227
Karyana Yandi <karyana@lokan.id>
2022-03-30 01:11:10 +0700

    refactor: delete tip column

--- dafunda-blocks.php
+++ dafunda-blocks.php
@@ -5,7 +5,7 @@
  * Description: Dafunda Custom Blocks
  * Plugin Author: Dafunda Dev Team
  * Author URI: https://dafunda.io
- * Version: 0.0.1-rc-2
+ * Version: 0.0.1-rc-3
  * Text Domain: dafunda-blocks
  * Domain Path: /languages
  *
