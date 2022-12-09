23a664988c4821009ea1565f814a28c4345e1795
beruangdev <beruangdev@gmail.com>
2022-12-09 23:16:27 +0700

    fix: review devided zero bug

--- includes/class-setup.php
+++ includes/class-setup.php
@@ -4,100 +4,109 @@ namespace DBE;
 use DBE\Activation;
 use DBE\Uninstall;
 
-final class Setup {
-	private static $instance;
-	public static $file_path;
+final class Setup
+{
+    private static $instance;
+    public static $file_path;
 
-	public static function instance() {
-		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Setup ) ) {
-			self::$instance = new Setup();
-			self::$instance->enqueue_dafunda_block();
-			self::$instance->enqueue_dafunda_block_sample();
-			self::$instance->enqueue_dafunda_block_asset();
+    public static function instance()
+    {
+        if (! isset(self::$instance) && ! (self::$instance instanceof Setup)) {
+            self::$instance = new Setup();
+            self::$instance->enqueue_dafunda_block();
+            self::$instance->enqueue_dafunda_block_sample();
+            self::$instance->enqueue_dafunda_block_asset();
 
             Activation::init();
             Uninstall::init();
-		}
-		return self::$instance;
-	}
+        }
+        return self::$instance;
+    }
 
-	public function enqueue_dafunda_block_asset() {
-		add_action(
-			'init',
-			function () {
-
-				$prefix = DBE_PREFIX;
-				wp_register_style(
-					'dbe-block-editor', // Handle.
-					plugins_url( "build/$prefix.blocks.editor.build.css", dirname( __FILE__ ) ),
-					array( 'wp-edit-blocks' ),
-					DBE_ASSET_VERSION
-				);
-			}
-		);
-
-		add_action(
-			'init',
-			function () {
-				// Register Style Front .
-				register_block_type(
-					'dafundacom/dafunda-blocks',
-					array(
-						'editor_style'  => 'dbe-block-editor',
-					)
-				);
-			}
-		);
-
-        add_action('wp_enqueue_scripts', function () {
-				$prefix = DBE_PREFIX;
-                wp_enqueue_style('dbe-block-style-front',plugins_url( "/build/$prefix.blocks.style.build.css", dirname( __FILE__ ) ), DBE_ASSET_VERSION, 'all');
+    public function enqueue_dafunda_block_asset()
+    {
+        add_action(
+            'init',
+            function () {
+                $prefix = DBE_PREFIX;
+                wp_register_style(
+                    'dbe-block-editor', // Handle.
+                    plugins_url("build/$prefix.blocks.editor.build.css", dirname(__FILE__)),
+                    array( 'wp-edit-blocks' ),
+                    DBE_ASSET_VERSION
+                );
             }
         );
 
-		return true;
-	}
+        add_action(
+            'init',
+            function () {
+                // Register Style Front .
+                register_block_type(
+                    'dafundacom/dafunda-blocks',
+                    array(
+                        'editor_style'  => 'dbe-block-editor',
+                    )
+                );
+            }
+        );
 
-	public function enqueue_dafunda_block() {
+        add_action(
+            'wp_enqueue_scripts',
+            function () {
+            $prefix = DBE_PREFIX;
+            wp_enqueue_style('dbe-block-style-front', plugins_url("/build/$prefix.blocks.style.build.css", dirname(__FILE__)), DBE_ASSET_VERSION, 'all');
+        }
+        );
+
+        return true;
+    }
+
+    public function enqueue_dafunda_block()
+    {
         $folders = $this->get_folders(DBE_PLUGIN_DIR . 'src/blocks');
-        foreach ( $folders as $key => $folder ) {
+        foreach ($folders as $key => $folder) {
             $path_block = DBE_PLUGIN_DIR . "build/blocks/$folder/block.php";
-            if ( file_exists($path_block) ) {
+            if (file_exists($path_block)) {
                 require $path_block;
             }
         }
-	}
+    }
 
-	public function enqueue_dafunda_block_sample() {
-		$folders = $this->get_folders(DBE_PLUGIN_DIR . 'src/sample-blocks');
-        foreach ( $folders as $key => $folder ) {
+    public function enqueue_dafunda_block_sample()
+    {
+        $folders = $this->get_folders(DBE_PLUGIN_DIR . 'src/sample-blocks');
+        foreach ($folders as $key => $folder) {
             $path = DBE_PLUGIN_DIR . "build/sample-blocks/$folder/index.php";
-            if ( file_exists($path) ) {
+            if (file_exists($path)) {
                 require $path;
             }
         }
-	}
+    }
 
-    public function get_folders( $path ) {
+    public function get_folders($path)
+    {
         $results = [];
-        foreach ( $this->directories_glob( $path ) as $item ) {
-         array_push($results, end(explode('/', $item)));
+        foreach ($this->directories_glob($path) as $item) {
+            $folder_end_name = explode('/', $item);
+            array_push($results, end($folder_end_name));
         }
 
         return $results;
     }
 
-    public function directories_glob( $directory ) {
-        $glob = glob( $directory . '/*' );
+    public function directories_glob($directory)
+    {
+        $glob = glob($directory . '/*');
 
-        if ( $glob === false ) {
+        if ($glob === false) {
             return array();
         }
 
         return array_filter(
             $glob,
-            function( $dir ) {
-                return is_dir( $dir );
+            function ($dir) {
+                return is_dir($dir);
             }
         );
     }
