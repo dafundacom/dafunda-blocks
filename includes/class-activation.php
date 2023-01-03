f2aab52503c16bcb6897f2cf37ddf25d2e7af879
beruangdev <beruangdev@gmail.com>
2023-01-03 18:07:09 +0700

    fix: perbaiki foreign post id prefix dbe log table

--- includes/class-activation.php
+++ includes/class-activation.php
@@ -16,31 +16,36 @@ class Activation
     public static function create_log_table()
     {
         global $wpdb;
-        $prefix = $wpdb->prefix . DBE_PREFIX;
+        // $prefix = $wpdb->prefix . DBE_PREFIX;
+        $prefix = DBE_PREFIX;
 
         $charset_collate = "";
         if (! empty($wpdb->collate)) {
             $charset_collate = "COLLATE `{$wpdb->collate}`";
         }
 
-        $tablename = "{$prefix}_vote_log";
-        
-        if ($wpdb->get_var("SHOW TABLES LIKE '$tablename'") != null) {
-            return true;
-        }
+        $tablename = "{$wpdb->prefix}{$prefix}_vote_log";
+        // if ($wpdb->get_var("SHOW TABLES LIKE '$tablename'") != null) {
+        //     return true;
+        // }
 
         $sql = "
-		CREATE TABLE `{$tablename}` (
+		CREATE TABLE if not exists `{$tablename}` (
 			`id` bigint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 			`post_id` bigint(20) unsigned NOT NULL,
 			`dbe_device_id` varchar(250) NOT NULL,
 			`block_id` varchar(250) NOT NULL,
 			`block_name` varchar(250) NOT NULL,
 			`action` varchar(250) NOT NULL,
-			FOREIGN KEY (`post_id`) REFERENCES `wp_posts` (`ID`) ON DELETE CASCADE
+			FOREIGN KEY (`post_id`) REFERENCES `{$wpdb->prefix}posts` (`ID`) ON DELETE CASCADE
 		) ENGINE=`InnoDB` {$charset_collate};";
 
-        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-        return maybe_create_table($wpdb->prefix . $tablename, $sql);
+        // require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        // return maybe_create_table($wpdb->prefix . $tablename, $sql);
+        $wpdb->get_var($sql);
+        // require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        // dbDelta($sql);
+
+        return true;
     }
 }
