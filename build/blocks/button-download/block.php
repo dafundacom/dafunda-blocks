de24e8486f5df55ef1a1fa0a16cb5d483f73501b
beruangdev <beruangdev@gmail.com>
2023-01-17 16:08:18 +0700

    feat: add posts block

---
+++ build/blocks/button-download/block.php
@@ -0,0 +1,16 @@
+<?php
+
+add_action('init', function() {
+    register_block_type(
+        __DIR__,
+        array(
+            'render_callback' => function($attributes, $content, $block_instance){
+                extract($attributes);
+                ob_start();
+                require plugin_dir_path(__FILE__) . './save/index.php';
+                require plugin_dir_path(__FILE__) . './save/schema.php';
+                return ob_get_clean();
+            },
+        )
+    );
+});
---
+++ build/blocks/recommendation/block.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'fa4eca442f82bd1e0286');
