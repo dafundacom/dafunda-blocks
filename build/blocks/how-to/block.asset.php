9b7448c9b092113d5f2cdcec8316b0acaf5dcf6d
beruangdev <beruangdev@gmail.com>
2023-07-22 15:22:49 +0700

    style: update style

--- build/blocks/how-to/block.asset.php
+++ build/blocks/how-to/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '977c965cdf381db55e43');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => 'cf3a08b5bd24b4b812bf');
